209edce3e109200b987324e1dd6abbb341264add
Govinda deb <gdeb089@gmail.com>
2013-04-16 19:51:55 +0530

    Improve

---
+++ src/com/gobinda/loginregistration/model/RegisterModel.java
@@ -0,0 +1,46 @@
+package com.gobinda.loginregistration.model;
+
+public class RegisterModel {
+	private String name,email,userName,password,contact_no;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getContact_no() {
+		return contact_no;
+	}
+
+	public void setContact_no(String contact_no) {
+		this.contact_no = contact_no;
+	}
+
+}
